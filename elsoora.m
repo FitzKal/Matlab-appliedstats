40c9cf8cd5de6a90fa2f836769d88f5222134302
FitzKal <169048358+FitzKal@users.noreply.github.com>
2024-09-19 13:43:58 +0200

    Add files via upload

---
+++ elsoora.m
@@ -0,0 +1,10 @@
+%unique(perms([1,1,2,3]), 'rows') %ismetleses permutacio
+
+factorial(5) * factorial (4) % 1.6
+
+factorial(7) / 7 %1.5
+
+factorial(8) / (factorial(5) * factorial(3)) %1.11
+
+%nchoosek(1,4) * nchoosek(9,48) + nchoosek(2,4) * nchoosek(8,48) + nchoosek(3,4) * nchoosek(7,48) + nchoosek(4,4) * nchoosek(6,48)
+
